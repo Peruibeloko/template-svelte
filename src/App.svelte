cf1ca975ee25a90d4a0714d467c38db9691c6469
peruibeloko <cmendonca32@gmail.com>
2023-10-12 21:06:48 -0300

    update repo

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import Content from "./lib/Content.svelte";
+</script>
+
+<header>Project name</header>
+<main><Content /></main>
+<footer>
+  <hr />
+  <a href="http://github.com/peruibeloko/template-svelte">Link to source</a>
+</footer>
+
+<style>
+  header,
+  main,
+  footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  main {
+    flex-grow: 1;
+  }
+
+  header,
+  footer {
+    height: 5rem;
+    width: 100%;
+  }
+
+  footer hr {
+    width: 80%;
+    color: var(--bg_s);
+  }
+
+  footer a {
+    margin-top: 1rem;
+    color: var(--aqua_dim);
+  }
+
+  footer a:hover {
+    color: var(--aqua);
+  }
+
+  * {
+    background-color: transparent;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+  }
+</style>
---
+++ src/lib/Content.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  let count: number = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+
+<style scoped>
+  button {
+  border: none;
+  color: var(--foreground);
+  background-color: transparent;
+  font-family: monospace;
+  font-size: 2rem;
+  border: 5px solid var(--main);
+  padding: 1rem;
+  border-radius: 1rem;
+  transition-duration: 200ms;
+}
+
+button:hover {
+  color: var(--background);
+  background-color: var(--foreground);
+}
+</style>
